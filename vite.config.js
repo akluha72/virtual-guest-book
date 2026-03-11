685afe25af3a6de52d6ef702c1682f0c96440f2f
lutfi_akmal <lutfiakmal@myra.com.my>
2026-03-11 16:08:43 +0800

    dev(layout): WIP layout fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from "vite";
 import { version } from "./package.json";
 
 export default defineConfig({
+    server: {
+        port: 5173,
+        allowedHosts: true, // allows all external hosts including ngrok
+    },
     define: {
         __APP_VERSION__: JSON.stringify(version),
         __BUILD_DATE__: JSON.stringify(new Date().toISOString().split("T")[0]), // YYYY-MM-DD
