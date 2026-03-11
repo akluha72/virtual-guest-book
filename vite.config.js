afe7ed1c04c0f903ecd2ccfb29817e3c6751d0c1
lutfii <lutfiakmal72@myra.com.my>
2026-03-11 21:57:36 +0800

    dev(layout): final section fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-// vite.config.js
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { version } from "./package.json";
 
 export default defineConfig({
+    plugins: [basicSsl()],
     server: {
+        host: true,
         port: 5173,
-        host: true,           // ← add this (binds to 0.0.0.0)
         allowedHosts: true,
+        https: true,
     },
     define: {
         __APP_VERSION__: JSON.stringify(version),
-        __BUILD_DATE__: JSON.stringify(new Date().toISOString().split("T")[0]), // YYYY-MM-DD
+        __BUILD_DATE__: JSON.stringify(new Date().toISOString().split("T")[0]),
     },
 });
